e527ae471b647dabe4d2573d169904016f775c79
Hasan Kanaya <hasan.kanaya9@gmail.com>
2024-08-11 02:42:33 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h3>This is just a test</h3>
+      <h4>Now let's test it out</h4>
+    </>
+  );
+}
+
+export default App;
